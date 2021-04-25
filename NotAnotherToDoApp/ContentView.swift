4b32af77e73609357f854f0d66748d73c0143c68
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-04-25 19:58:00 +0300

    Initial Commit

---
+++ NotAnotherToDoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotAnotherToDoApp
+//
+//  Created by Ramill Ibragimov on 25.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotAnotherToDoApp/NotAnotherToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotAnotherToDoAppApp.swift
+//  NotAnotherToDoApp
+//
+//  Created by Ramill Ibragimov on 25.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotAnotherToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
